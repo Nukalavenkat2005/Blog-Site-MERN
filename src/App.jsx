2050898d9bde6ba8bf7e42c388411651e1f9a3ea
satyanarayana <nukala.venkat2005@gmail.com>
2025-03-31 13:01:13 +0530

    Updated NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
 
 const App = () => {
   return (
     <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
-=======
-const App = () => {
-  return (
-    <div className='px-4 md :px-8 lg:px-16 lx:px-32 2xl:px-64'> 
->>>>>>> 26240f5 (Added full-stack-blog project)
       {/* NAVBAR */}
       <Navbar />
       {/* BREADCRUMB */}
@@ -16,12 +10,7 @@ const App = () => {
       {/* FEATURED POSTS */}
       {/* POST LIST */}
     </div>
-<<<<<<< HEAD
   );
 };
-=======
-  )
-}
->>>>>>> 26240f5 (Added full-stack-blog project)
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Image from "./Image";
@@ -11,7 +10,7 @@ const Navbar = () => {
       {/* Logo */}
       <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image src="logo.png" alt ="lama logo" w={32} h ={32}/>
-        <span>Lamalog</span>
+        <span>VBlog</span>
       </Link>
 
       {/* Mobile Menu */}
@@ -62,35 +61,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-=======
-import { useState } from "react"
-
- const Navbar = () => {
-  const [open,setopen] = useState(false);
-   return (
-    
-     <div className="w-full h-16 md:h-20 flex items-center justify-between">
-        {/*logo */}
-        <div className="flex items-center gap-4 text-2xl font-bold">
-            <img src="/logo.png" className="w-8 h-8" alt="" />
-            <span>Lamalog</span>
-        </div>
-        {/*mobile menu*/}
-        <div className="md:hidden">
-           {/* MOBILE BUTTON */}
-          <div className="cursor-pointer text-4xl" onClick={()=> setopen((prev)=>!prev)}>{open?"X": "☰"}
-
-          </div>
-          <div className={`w-full h-screen bg-[#e6e6ff] flex flex-col items-center justify-center  absolute top-16 bg-[#398cbd] transition-all ease-in-out ${open ? "-right-0":"-right-[100%]"}`}>
-            Menu
-          </div>
-        </div>
-        {/*Desktop menu*/}
-        <div className="hidden md :flex">D</div>
-
-     </div>
-   )
- }
- 
- export default Navbar
->>>>>>> 26240f5 (Added full-stack-blog project)
